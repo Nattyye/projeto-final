b57a5aff3a3d32c04eb57d2d9e80369a4ece47e3
Natalia <132146090+Nattyye@users.noreply.github.com>
2023-08-12 16:43:39 -0300

    Add files via upload
    Um quiz de Naruto para testar sus conhecimentos :)

---
+++ projeto-final-natalia-cristine/questions.js
@@ -0,0 +1,101 @@
+export default [
+    {
+      question: "Quantos episódios tem Naruto Clássico?",
+      answers: [
+        { option: "257", correct: false },
+        { option: "220", correct: true },
+        { option: "312", correct: false },
+        
+      ],
+    },
+    {
+      question: "Quem é o criador da obra?",
+      answers: [
+        { option: "Koyoharu Gotouge", correct: false },
+        { option: "Kohei Horikoshi", correct: false },
+        { option: "Masashi Kishimoto", correct: true },
+      ],
+    },
+    {
+      question: "Quem é o personagem considerado o deus shinobi?",
+      answers: [
+        { option: "Senju Tobirama", correct: false },
+        { option: "Namikase Minato", correct: false },
+        { option: "Senju Hashirama", correct: true },
+      ],
+    },
+    {
+      question: "Qual é o nome do shodai kazekage?",
+      answers: [
+        { option: "Reto", correct: true },
+        { option: "Rasa", correct: false },
+        { option: "Gaara", correct: false },
+       
+      ],
+    },
+    {
+      question: "Quem foi o primeiro vilão de Naruto Clássico?",
+      answers: [
+        { option: "Zabuza Momochi", correct: false },
+        { option: "Mizuki", correct: true },
+        { option: "Haku", correct: false },
+      ],
+    },
+    {
+      question: "Qual foi o personagem mais novo a se tornar hokage?",
+      answers: [
+        { option: "Sarutobi Hiruzen", correct: true },
+        { option: "Uzumaki Naruto", correct: false },
+        { option: "Hatake Kakashi", correct: false },
+      ],
+    },
+    {
+      question: "Quantos anos Uchiha Madara tinha quando morreu?",
+      answers: [
+        { option: "98 anos", correct: false },
+        { option: "121 anos", correct: true },
+        { option: "Ele não morreu", correct: false },
+      ],
+    },
+    {
+      question: "Durante o arco de Pain/Nagato, quantas caudas Naruto libera?",
+      answers: [
+        { option: "8 caudas", correct: true },
+        { option: "5 caudas", correct: false },
+        { option: "2 caudas", correct: false },
+      ],
+    },
+    {
+      question: "Quais os 5 elementos de chakra?",
+      answers: [
+        { option: "Vento,raio,terra,fogo e água", correct: false },
+        { option: "Fogo,trovão,gelo,vento e cristal", correct: false },
+        { option: "Fogo,vento,luz,terra e água", correct: true },
+      ],
+    },
+    {
+      question: "Qual tipo sanguíneo de Uchiha Madara?",
+      answers: [
+        { option: "O", correct: true },
+        { option: "A", correct: false },
+        { option: "B", correct: false },
+      ],
+    },
+    {
+      question: "Quem é o projenitor(a) do Chakra?",
+      answers: [
+        { option: "Otsutsuki Shibai", correct: false },
+        { option: "Otsutsuki Hagoromo", correct: false },
+        { option: "Otsutsuki Kaguya", correct: true },
+      ],
+    },
+    {
+      question: "Quem criou a Akatsuki?",
+      answers: [
+        { option: "Uchiha Obito", correct: false },
+        { option: "Yahiko", correct: true },
+        { option: "Uchiha Madara", correct: false },
+      ],
+    },
+  ];
+    
